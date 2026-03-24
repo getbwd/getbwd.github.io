bc0c812193267680543059840a3b45a5b517b74d
getbwd <brotherswhomow@gmail.com>
2026-03-24 09:51:23 -0400

    1

--- just/announcement.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const bannerHTML = `
-    <div class="announcement-bar">
-        Save time and unlock exclusive online pricing by creating an account.
-    </div>
-    `;
-
-    document.body.insertAdjacentHTML("afterbegin", bannerHTML);
-});
